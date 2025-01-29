7955a1e3cdbe2a7e67dba66f709a6a64f7607e8a
AminiMehrdad <mehrdadamini1400@gmail.com>
2025-01-29 19:16:53 +0330

    complit db schema

--- database/models/articles.js
+++ database/models/articles.js
@@ -3,13 +3,25 @@ const mongoose = require("mongoose");
 const ArticleSchema = new mongoose.Schema({
     Title: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "Title is required"],
+        unique: [true, "UserName shold be unique"],
+        validate: {
+            validator: function(titel) {
+                return titel && title.length > 3;
+            },
+            message: `Title must be at least 4 characters long`
+        }
     },
     Text: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "text is required"],
+        unique: [true, "text mostbe unique"],
+        validate: {
+            validator: function(text) {
+                return text && text.length > 10;
+            },
+            massage: "Text must be at least 11 characters long"
+        }
     },
     CreateAt: {
         type: Date,
@@ -18,6 +30,10 @@ const ArticleSchema = new mongoose.Schema({
     NumberSee: {
         type: Number,
         default: 0
+    },
+    ID_creator: {
+        type: String,
+        required: true
     }
 });
 
--- database/models/users.js
+++ database/models/users.js
@@ -1,27 +1,42 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
     FerstName: {
         type: String,
-        required: true
+        required: [true, "FerstName is required"],
+        minlength: [3, "Name must be at least 3 characters long"],
+        maxlength: [50, "Name cannot exceed 50 characters"]
     },
     LastName: {
         type: String,
-        required: true
+        required: [true, "LastName is required"],
+        minlength: [3, "LastName must be at least 3 characters long"],
+        maxlength: [50, "LastName cannot exceed 50 characters"]
     },
     UserName: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "UserName is required"],
+        unique: [true, "UserName shold be unique"],
+        minlength: [3, "UserName must be at least 3 characters long"],
+        maxlength: [50, "UserName cannot exceed 50 characters"]
     },
     PassWord: {
         type: String,
-        required: true
+        required: [true, "PassWord is required"],
+        minlength: [4, "PassWord must be at least 3 characters long"],
+        maxlength: [20, "PassWord cannot exceed 20 characters"]
     },
     PhoneNumber: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "Phone Number is requrie"],
+        unique: [true, "PhoneNUmber shold be unique"],
+        validate: {
+            validator: function(value) {
+                return validator.isMobilePhone(value, "fa-IR");
+            },
+            message: (props) => `${props.value} is not a valid phone number!`,
+        },
     },
     Gender: {
         type: String,
